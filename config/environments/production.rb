a0fcee8e400c1987345bd2eff33a4335bde0f0c3
Carlos Manuel <geko727@gmail,com>
2013-11-19 13:08:39 -0600

    correo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,9 +64,9 @@ Bbbuy::Application.configure do
 config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'example.com',
-  user_name:            '<username>',
-  password:             '<password>',
+  domain:               'warm-island-6010.heroku.com',
+  user_name:            'bbbuy.codecery@gmail.com',
+  password:             'bbbuy.codecery',
   authentication:       'plain',
   enable_starttls_auto: true  }
 
