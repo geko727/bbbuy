a082dbb609693b4cca342986a4f65a649ad59d18
Carlos Manuel <geko727@gmail,com>
2013-11-20 09:53:36 -0600

    timezone

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,9 @@ Bundler.require(:default, Rails.env)
 
 module Bbbuy
   class Application < Rails::Application
+
+    config.time_zone = 'Mexico City' 
+    config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
