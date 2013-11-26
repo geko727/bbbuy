e24ddf504c48e901384852bbb59f341b8ee8b5b4
Carlos Manuel <manuel@codecery.com>
2013-11-26 10:11:07 -0600

    comment

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -1,7 +1,7 @@
 class SeriesController < ApplicationController
   before_action :set_series, only: [:show, :edit, :update, :destroy]
   before_filter :require_user, only: [:show, :edit, :update, :destroy, :index, :new]
-  
+  #comment
   # GET /series
   # GET /series.json
   def index
